990b88635f557aae1febbc2ecaa01e3ec74d7ad5
cyb <2439651385@qq.com>
2025-11-19 17:54:47 +0800

    main

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,20 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import '@/styles/base.scss'
 import App from './App.vue'
 import router from './router'
+import { useUserStore } from '@/stores/userStore'
 
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+app.use(pinia)
 app.use(router)
+app.use(ElementPlus)
+
+const userStore = useUserStore(pinia)
+userStore.init()
 
 app.mount('#app')
