be9dfa131e6e1af290687a568241a2faa6f97e40
cyb <2439651385@qq.com>
2025-11-19 11:17:53 +0800

    路由配置

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+
+
+// 路由配置
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    redirect: '/login',
+  },
+  {
+    path: '/login',
+    component: () => import('@/layouts/AuthLayout.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Login',
+        component: () => import('@/views/auth/LoginPage.vue'),
+        meta: {
+          title: '登录',
+        },
+      },
+    ],
+  },
+  {
+    path: '/register',
+    component: () => import('@/layouts/AuthLayout.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Register',
+        component: () => import('@/views/auth/RegisterPage.vue'),
+        meta: {
+          title: '注册',
+        },
+      },
+    ],
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
