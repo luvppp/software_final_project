70becc0584f02bad7fc0805c087903f4d03f64d9
cyb <2439651385@qq.com>
2025-11-19 11:17:07 +0800

    入口

---
+++ frontend/src/layouts/AuthLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="auth-layout">
+    <router-view />
+  </div>
+</template>
+
+<script setup lang="ts">
+// 认证布局组件，用于登录、注册等页面
+</script>
+
+<style scoped lang="scss">
+.auth-layout {
+  min-height: 100vh;
+  width: 100%;
+}
+</style>
